d9a7b48451a9dbe5f43155cfe9c61e2fa8390cf3
SaiPh3r <saianand750@gmail.com>
2025-09-07 13:14:02 +0530

    Create simple_chain.py

---
+++ simple_chain.py
@@ -0,0 +1,20 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatGoogleGenerativeAI(model='gemini-1.5-flash')
+
+parser = StrOutputParser()
+
+prompt = PromptTemplate(
+    template='generate 5 facts on the {topic}' , 
+    input_variables=['topic']
+)
+
+chain = prompt | model | parser
+
+result = chain.invoke({'topic':'chain vs non-chain parser methds only syntax wise'})
+print(result)
